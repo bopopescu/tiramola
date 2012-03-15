b1e5ad906450a82b35b5dcd50c0db93b3dbd424d
vagoskar <vagoskar@gmail.com>
2012-03-15 11:29:20 +0000

    Updating TIRAMOLA to v2 from local repository

--- TIRAMOLA/src/gr/ntua/cslab/eMRE/templates/riak/configure-riak.sh
+++
@@ -1,6 +0,0 @@
-sed "s/0.0.0.0/$1/g" /etc/riaksearch/app.config > /etc/riaksearch/app.config.temp
-sed "s/0.0.0.0/$1/g" /etc/riaksearch/vm.args > /etc/riaksearch/vm.args.temp
-mv /etc/riaksearch/app.config.temp /etc/riaksearch/app.config
-mv /etc/riaksearch/vm.args.temp /etc/riaksearch/vm.args
-/usr/sbin/riaksearch-admin reip riak@0.0.0.0 riak@$1
-/usr/sbin/riaksearch start
